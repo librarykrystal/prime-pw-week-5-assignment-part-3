ee0fad22ee99d591fd2461214c62b06c66251ab6
Krys Frost <librarykrystal@gmail.com>
2022-10-28 22:13:12 -0500

    AddToCollection object constructor function written.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+
+let collection = [];
+
+function AddToCollection(title, artist, yearPublished){
+    this.title = title;
+    this.artist = artist;
+    this.yearPublished = yearPublished;
+    collection.push(this);
+    return this;
+}
+
+let indigos = new AddToCollection('Ellington Indigos', 'Duke Ellington', 1958);
+let raindogs = new AddToCollection('Raindogs', 'Tom Waits', 1985);
+
+// TESTED console.log(indigos);
+console.log('Current collection:', collection);
