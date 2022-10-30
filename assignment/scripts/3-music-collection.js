d60d3701f5bb0d749abe8eab2f761bbdf88cb943
Krys Frost <librarykrystal@gmail.com>
2022-10-29 20:37:59 -0500

    findByArtist function completed and tested.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,10 +9,11 @@ function AddToCollection(title, artist, yearPublished){
     this.yearPublished = yearPublished;
     collection.push(this);
     return this;
-}
+}   // end of AddToCollection
 
 let indigos = new AddToCollection('Ellington Indigos', 'Duke Ellington', 1958);
 console.log('Album Test / Indigos', indigos);
+// Used first album as in-prog function writing tester before adding add'l albums
 let alice = new AddToCollection('Alice', 'Tom Waits', 2002);
 console.log('Album Test / Alice', alice);
 let nationalRansom = new AddToCollection('National Ransom', 'Elvis Costello', 2010);
@@ -28,10 +29,29 @@ console.log('Current collection:', collection);
 
 
 function showCollection(collectionArray){
-    console.log(collectionArray.length);
+    console.log('Albums in collection:', collectionArray.length);
     for(eachAlbum of collectionArray){
         console.log(eachAlbum.title, 'by', eachAlbum.artist, 'published in', eachAlbum.yearPublished);
     }
-}
+}   // end of showCollection
 
+console.log('Calling the showCollection function:');
 showCollection(collection);
+
+
+function findByArtist(artist){
+    let artistResults = [];
+    for(eachArtist of collection){
+        if(eachArtist.artist == artist){
+            artistResults.push(eachArtist);
+        }
+    }
+    return artistResults;
+}   // end of findByArtist
+
+// Test for artist with 1 album in array:
+console.log('Search for OutKast:', findByArtist('OutKast'));
+// Test for artist with 2 albums in array:
+console.log('Search for Elvis Costello:', findByArtist('Elvis Costello'));
+// Test for artist NOT in array:
+console.log('Search for artist not in collection:', findByArtist('Yanni'));
